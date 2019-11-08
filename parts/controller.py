6d7f83725ad7a4f24cc78179714dda4ab2911be0
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2019-11-08 09:20:13 +0900

    ステアリング方向修正

--- parts/controller.py
+++ parts/controller.py
@@ -65,15 +65,11 @@ class ELECOM_JCU3912TController(JoystickController):
             
         self.button_down_trigger_map = {
             # 右ボタン群：上
-            #'1': self.on_recording,
             '1': self.normal_stop,
-            #'2': self.off_recording,
             '2': self.erase_last_N_records,
 
             # 右ボタン群：下
-            #'3': self.set_user_init,
             '3': self.emergency_stop,
-            #'4': self.set_local_init,
             '4': self.toggle_manual_recording,
 
             # トリガ
@@ -87,14 +83,12 @@ class ELECOM_JCU3912TController(JoystickController):
             # アナログスティック押込   
             '9': self.normal_stop,
             '10': self.normal_stop,
-            #'10': self.erase_last_N_records,
+
+            # SELECT 相当
+            '11': self.toggle_mode,
 
             # START相当
-            #'12': self.toggle_mode,
             '12': self.toggle_constant_throttle,
-            # SELECT 相当
-            #'11': self.toggle_manual_recording,
-            '11': self.toggle_mode,
         }
 
         self.button_up_trigger_map = {
@@ -103,32 +97,12 @@ class ELECOM_JCU3912TController(JoystickController):
 
         self.axis_trigger_map = {
             'analog_left_vertical': self.set_throttle,
-            'analog_left_horizontal': self.set_steering_analog,
+            'analog_left_horizontal': self.set_steering,
             'analog_right_vertical': self.set_throttle,
-            'analog_right_horizontal': self.set_steering_analog,
+            'analog_right_horizontal': self.set_steering,
             'dpad_horizontal': self.move_left_or_right,
             'dpad_vertical': self.move_front_or_rear,
         }
-
-    def set_user_init(self):
-        self.mode = 'user'
-        print('force mode:', self.mode)
-    
-    def set_local_init(self):
-        self.mode = 'local'
-        print('force mode:', self.mode)
-    
-    def on_recording(self):
-        if self.auto_record_on_throttle:
-            print('auto record on throttle is enabled.')
-        self.recording = True
-        print('recording:', self.recording)
-
-    def off_recording(self):
-        if self.auto_record_on_throttle:
-            print('auto record on throttle is enabled.')
-        self.recording = False
-        print('recording:', self.recording)
 
     def normal_stop(self):
         self.set_throttle(0)
@@ -138,10 +112,10 @@ class ELECOM_JCU3912TController(JoystickController):
         
         if axis_val > 0:
             self.set_throttle(-1)
-            self.set_steering(-1)
+            self.set_steering(1)
         elif axis_val < 0:
             self.set_throttle(-1)
-            self.set_steering(1)
+            self.set_steering(-1)
         else:
             self.set_throttle(0)
             self.set_steering(0)
